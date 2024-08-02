9ffbd7d88da55622b2659a1ba5d04f468ec5fe78
DIbarguenCurrea <dibarguenc@gmail.com>
2024-08-02 12:43:19 +0200

    Creating Sidebar

--- front/src/components/Sidebar/Sidebar.jsx
+++ front/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 function Sidebar() {
-  return <h1 className="text-3xl text-red-600">Esto es el Sidebar</h1>;
+  return (
+    <div className="w-1/6 h-screen border border-red-600">
+      <p>
+        Primero colocar el botón burguer que hara que se muestre o no el panel
+      </p>
+      <h1 className="text-4xl">Dashboard</h1>
+      <h3>Home</h3>
+      <h3>Invoice</h3>
+      <h3>Contacts</h3>
+      <h3>Message</h3>
+      <h4 className="">Log Out</h4>
+    </div>
+  );
 }
 
 export default Sidebar;
